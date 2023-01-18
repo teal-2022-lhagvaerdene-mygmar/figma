99f443ee35dee5229506639f4a1fcdf03aa8d94e
teal-2022-lhagvaerdene-mygmar <teal.2022.lhagvaerdene.mygmar@gmail.com>
2023-01-18 17:32:47 +0800

    01-18

--- src/header component/homeSearch.js
+++ src/header component/homeSearch.js
@@ -3,22 +3,38 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 export function HomeSearch() {
   return (
-    <section className="card-sec">
-      <div>
-        <img src="logo 1.png" alt="logo" />
+    <section className="card-sec d-flex p-4 justify-content-between">
+      <div className="d-flex card-div">
+        <div>
+          <img src="logo 1.png" alt="logo" />
+        </div>
+        <div>
+          <input placeholder="search" />
+          <button>search</button>
+        </div>
+        <div>
+          <img src="Vector2.png" alt="s" className="bg-ligth" />
+        </div>
       </div>
       <Search />
-      <div>
-        <img src="Vector2.png" alt="s" className="bg-ligth" />
-      </div>
     </section>
   );
 }
 function Search() {
   return (
-    <div>
-      <input placeholder="search" />
-      <button>search</button>
+    <div className="d-flex text-light card-last">
+      <div className="d-flex card-last-div">
+        <div>
+          <img src="user.png" alt="user" />
+          Sign in
+        </div>
+        <div>
+          <img src="heart.png" alt="heart" />0
+        </div>
+        <div>
+          <img src="shopping-cart.png" alt="shopping-cart" />0
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/header component/itemList.js
@@ -0,0 +1,12 @@
+export function ItemList() {
+  return (
+    <div>
+      <div>
+        <h2>Canon</h2>
+        <h2>Camera</h2>
+        <button>Shop now</button>
+        <button>view more</button>
+      </div>
+    </div>
+  );
+}
